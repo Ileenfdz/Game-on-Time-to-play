d53c8d857beb1aa726bcd56ab29a7d00639948eb
Ileenfdz <ileenfdz@gmail.com>
2022-03-23 01:32:16 +0100

    Last commit of the 'night', need to install jest and make testing of the functions I'm doing, update the enemy randomly

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 let character = document.getElementById("character");
 let block = document.getElementById("block");
-let random = Math.floor(Math.random() * 10) + 1;
+let randomCharacter = Math.floor(Math.random() * 10) + 1;
 
-if (random <= 5) {
+if (randomCharacter <= 5) {
     character.style.backgroundColor = "rgb(221, 33, 33)";
     block.style.backgroundColor = "rgb(161, 115, 30)";
 }
-if (random > 5) {
+if (randomCharacter > 5) {
     character.style.backgroundColor = "rgb(37, 89, 201)";
     block.style.backgroundColor = "rgb(63, 139, 240)";
 }
@@ -25,49 +25,65 @@ setInterval(function () {
 
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let randomEnemy = Math.floor(Math.random() * 3) + 1;
 
-    if (random > 5) {
+    if (randomCharacter > 5) {
         if (characterTop < 130) {
             character.style.backgroundColor = "rgb(87, 163, 250)";
         } else {
             character.style.backgroundColor = "rgb(43, 49, 217)";
         }
-    } else if (random <= 5) {
+
+        if(randomEnemy == 1){
+            block.style.backgroundColor = "rgb(rgb(22, 172, 72))"
+        }else if(randomEnemy == 2){
+            block.style.backgroundColor = "rgb(rgb(161, 115, 30))"
+        }else if(randomEnemy == 3){
+            block.style.backgroundColor = "rgb(rgb(235, 229, 151))"
+        }
+
+    } else if (randomCharacter <= 5) {
 
         if (characterTop < 130) {
             character.style.backgroundColor = "rgb(247, 47, 47)";
         } else {
             character.style.backgroundColor = "rgb(156, 11, 11)";
         }
-    }
 
-    //
+        if(randomEnemy == 1){
+            block.style.backgroundColor = "rgb(rgb(76, 228, 127))"
+        }else if(randomEnemy == 2){
+            block.style.backgroundColor = "rgb(rgb(63, 139, 240))"
+        }else if(randomEnemy == 3){
+            block.style.backgroundColor = "rgb(rgb(233, 54, 54))"
+        }
+
+    }
 
     // if(blockLeft > )
 
-    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
-        block.style.animation = "none";
-        block.style.display = "none";
-        showAlert("Sorry, you lost");
-    }
+    // if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+    //     block.style.animation = "none";
+    //     block.style.display = "none";
+    //     showAlert("Sorry, you lost");
+    // }
 
 }, 10);
 
 
 function showAlert(message) {
     alert(message);
+    window.location.reload();
 }
 
 //Ideas
 //-Dependiendo del tiempo, en el alert le saldrá una puntuación (por cada sec + xnum)
 
-//-A partir de x tiempo, si no perdió, cambia el fondo a oscuro y los colores si es necesario
-
 //-En el modo "claro", Mario con, Goomba(marrón), Koopa(verde) y Boo (blanco/claro)
 //-En el modo "oscuro", Ash con, piplup(azul), turtwig(verde) y chimchar(rojo)
 
 //-Hacer diferentes enemigos (primero saldrán solo diferentes colores)
-//-Cuando esto esté, que aparezcan de forma random
+//-Cuando esto esté, que aparezcan de forma randomCharacter
 
 //  rgb(37, 89, 201) Ash
 //  rgb(25, 66, 153) Ash
